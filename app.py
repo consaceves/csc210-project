313bafabc2e94880c1721eecaf064685510f7b91
waynexu1998 <cobrawaynexu@gmail.com>
2019-11-14 10:49:22 -0500

    Changed app.py to move definition functions outside main.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
 from api import auth
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oEHYBreJ2QSefBdUhD19PkxC"
+app.register_blueprint(auth.app)
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "oEHYBreJ2QSefBdUhD19PkxC"
-    app.register_blueprint(auth.app)
+    # app = Flask(__name__)
     app.run()
