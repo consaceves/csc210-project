baaff98f0a40a7a8bee06097a0e7eaf9277d5aea
Constanza Aceves Rodriguez <cacevesr@u.rochester.edu>
2019-11-14 15:45:59 -0500

    Fix style for app file

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
 from api import auth
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oEHYBreJ2QSefBdUhD19PkxC"
+
+# Blueprints
 app.register_blueprint(auth.app)
 
 if __name__ == "__main__":
