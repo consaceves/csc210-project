f44dfedf786fa0299911dbf7cbe629608b4f1bf6
Constanza Aceves Rodriguez <cacevesr@u.rochester.edu>
2019-11-16 08:49:18 -0500

    Add models file and db info

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from api import auth
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oEHYBreJ2QSefBdUhD19PkxC"
+
+app.config["SECRET_KEY"] = "oEHYBreJ2QSefBdUhD19PkxC"
+appdir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    f"sqlite:///{os.path.join(appdir, 'ebay2.db')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Blueprints
 app.register_blueprint(auth.app)
