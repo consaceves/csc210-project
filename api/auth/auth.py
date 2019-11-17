5e19f0206432e91299acc6b0e23d07a81d2ac905
Constanza Aceves Rodriguez <cacevesr@u.rochester.edu>
2019-11-16 20:00:09 -0500

    Add hidden route and display username

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -16,11 +16,9 @@ def home():
 def createaccount():
     form = CreateAccountForm()
     if form.validate_on_submit():
-        print("VALIDATED")
         id = randint(1, 100)
         username = form.username.data
         password = form.password.data
-        print(username, password)
 
         new_user = models.User(id=id, username=username, password=password)
         db.session.add(new_user)
@@ -32,17 +30,15 @@ def createaccount():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("home_page.home"))
+        return render_template("hidden.html", user=current_user.username)
     form = LoginForm()
     if form.validate_on_submit():
         user = models.User.query.filter_by(username=form.username.data).first()
-        print(user)
-        print(form.username.data)
         if user is None or not user.verify_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for("home_page.login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("home_page.home"))
+        return render_template("hidden.html", user=current_user.username)
     return render_template("login.html", form=form)
 
 
